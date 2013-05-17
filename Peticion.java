66bf67f5e1f3a9833303e4745b26a8bf037bf75d
German Leon <cronos@alpha>
2013-05-17 18:09:49 -0430

    Cambios en el simulador

--- Peticion.java
+++ Peticion.java
@@ -1,9 +1,10 @@
+import java.
+
 public class Peticion {
-    //  nt tiempo;         //  Momento en que se hizo la peticion
-    private int inicial;        //  bloque inicial
-    private int cantidad;       //  bloques a leer o escribir
-    private int tipo;           //  tipo de peticion (Lectura o escritura)
-    private Prio prioridad;      //  tipo de prioridad 
+    private int tiempo;                 //  Momento en que se hizo la peticion
+    private LinkedList<int> bloques;    //  bloque inicial
+    private int tipo;                   //  tipo de peticion (Lectura o escritura)
+    private Prio prioridad;             //  tipo de prioridad 
 
     private enum Prio {
         RT, BE, IDLE
--- Simulador.java
+++ Simulador.java
@@ -1,30 +1,54 @@
 public class Simulador {
     Disco d;
+    static int tamano_bloque = 4096;
+
+    public Simulador(Disco d) {
+        this.d = d;
+    }
 
     private Disco parseDisco(String archivo_config) {
-
+        //  Matteo
     }
 
     private int buscarSectorParaBloque(int bloque) {
+        return (bloque * 8) + 1;
+    }
 
+    private Cabezal buscarCabezal(int sector) {
+        for (Cabezal c: d.cabezales) {
+            if (c.min <= sector && sector < c.max)
+                return c;
+        }
     }
 
     private long getLatency(Peticion p) {
+        int total;
 
+        LinkedList<int> sectores_pendientes = new LinkedList<int>();
+        int num_sector;
+        for (int b: p.bloques) {
+            num_sector = buscarSectorParaBloque(p.inicial);
+            sectores_pendientes.add(num_sector);
+        }
+
+        Collections.sort(sectores_pendientes);
+
+        long tiempo_por_sector = 0;
+        for (int sector: sectores_pendientes) {
+            tiempo_por_sector += moveCabezal(buscarCabezal(sector));    
+            tiempo_por_sector += procesarSector(sector);    
+            total += tiempo_por_sector;
+        }
+        return aux1 + aux2;
     }
 
-    private sectorToTrack(int sector) {
-
+    private int sectorToTrack(int sector) {
     }
 
-    private long moveCabezal(int track) {
-
+    private long moveCabezal(Cabezal c, int track) {
+        
     }
 
     private long getTiempo (int tipo) {
-
-    }
-
-    public static void main (String [] args) {
     }
 }
