d2cec7a2e4da8ac5f61f975edccd10d2c6f5930d
German Alejandro Leon Zapata <08-10611@ldc.usb.ve>
2013-05-20 09:09:30 -0430

    Cambios en las clases para procesar las peticiones

--- Disco.java
+++ Disco.java
@@ -5,15 +5,52 @@ public class Disco {
     int capacidadDisco;     // 
     int capacidadPlatos;
     int tamanoSector;
+    int trackActual;
     int densidad;
     ArrayList<Cabezal> cabezales;
+
     int rpm;
-    int tfreno;
-    int tarranque;
-    int tasaLectura;
-    int tasaEscritura;
+    int averageSeekTime;
+    float latenciaRotacional;
+    int tasaLectura;            // MB / milisegundos
+    int tasaEscritura;          // MD / milisegundos
 
-    public Disco() {
+    public Disco(int cs, int cd, int cp, int ts, int ta, int d,
+                 ArrayList<Cabezal> cbs, int rpm, int ast, float lr,
+                 int tl, int te) {
+        this.cilindros = cs;
+        this.capacidadDisco = cd;
+        this.capacidadPlatos = cp;
+        this.tamanoSector = ts;
+        this.trackActual = ta;
+        this.densidad = d;
+        this.cabezales = cbs;
+        this.rpm = rpm;
+        this.averageSeekTime = ast;
+        this.latenciaRotacional = 60 / this.rpm;
+        this.tasaLectura = tasaLectura / 1000;
+        this.tasaEscritura = tasaEscritura / 1000;
+    }
 
+    public long moverBrazo(int trackDestino) {
+        this.trackActual = trackDestino;
+        return Math.abs(trackDestino - trackActual) * averageSeekTime;
+    }
+
+    public Cabezal buscarCabezal(int sector) {
+            for (Cabezal c: this.cabezales) {
+                if (c.min <= sector && sector < c.max)
+                    return c;
+            }
+            return null;
+        }
+
+    public long procesarSector(int sector, char tipo) {
+        long total = 0;
+        if (tipo == 'r') 
+            total = sector / tasaLectura;
+        else if (tipo == 'w')
+            total = sector / tasaEscritura;
+        return total;
     }
 }
--- Peticion.java
+++ Peticion.java
@@ -1,13 +1,25 @@
-import java.
+import java.util.LinkedList;
 
 public class Peticion {
     private int tiempo;                 //  Momento en que se hizo la peticion
-    private LinkedList<int> bloques;    //  bloque inicial
-    private int tipo;                   //  tipo de peticion (Lectura o escritura)
+    private LinkedList<Integer> bloques;    //  bloque inicial
+    private char tipo;                   //  tipo de peticion (Lectura o escritura)
     private Prio prioridad;             //  tipo de prioridad 
 
     private enum Prio {
         RT, BE, IDLE
+    }
+
+    public int getTiempo() {
+        return this.tiempo;
+    }
+
+    public LinkedList<Integer> getBloques() {
+        return this.bloques;
+    }
+
+    public char getTipo() {
+        return this.tipo;
     }
 }
 
