b7b4c960398effdf0a6671ed76f9ba65ce3267a3
German Leon <cronos@alpha>
2013-05-22 23:51:16 -0430

    Cambios a las clases

--- Disco.java
+++ Disco.java
@@ -1,34 +1,25 @@
 import java.util.List;
 import java.util.ArrayList;
-// Imports para lectura facil de XML
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import java.io.File;
 
 public class Disco {
-    int capacidadDisco;         //  Tamano en bytes del disco duro
-    int capacidadPlatos;        //  Tamano en bytes de cada plato
-    int tamanoSector;           //  Tamano en bytes de cada sector del dd
-    int trackActual;            //  Track actual en la que esta posicionado el brazo
-    int densidad;
-    List<Cabezal> cabezales;
+    int capacidadDisco;             //  Tamano en bytes del disco duro
+    int capacidadPlatos;            //  Tamano en bytes de cada plato
+    int tamanoSector;               //  Tamano en bytes de cada sector del dd
+    int trackActual;                //  Track actual en la que esta posicionado el brazo
+    int densidad;                   //  Cantidad de tracks por centimetro de radio del plato
+    List<Cabezal> cabezales;        //  Lista de cabezales del disco duro
 
-    int rpm;                    //  Revoluciones por minuto de los platos
-    int averageSeekTime;        //  Tiempo promedio de mover el cabezal de un track al siguiente
-    float latenciaRotacional;   //  Tiempo promedio de espera para que leer o escribir un sector
-    int tasaLectura;            //  MB / milisegundos
-    int tasaEscritura;          //  MB / milisegundos
-	ArrayList<Peticion> peticiones;
+    int rpm;                        //  Revoluciones por minuto de los platos
+    int averageSeekTime;            //  Tiempo promedio de mover el cabezal de un track al siguiente
+    float latenciaRotacional;       //  Tiempo promedio de espera para que leer o escribir un sector
+    int tasaLectura;                //  tasa promedio de lectura del disco duro MB / milisegundos 
+    int tasaEscritura;              //  tasa promedio de escritura del disco duro MB / milisegundos
 
-    int diametroPlato;
-    int sectoresPorTrack;
+    int diametroPlato;              //  Diametro de cada plato del disco duro, medido en centimetros
+    int sectoresPorTrack;           //  Cantidad de sectores existentes en cada track del disco duro
 
     public Disco(int cs, int cd, int cp, int ts, int ta, int d,
-                 ArrayList<Cabezal> cbs, int rpm, int ast, float lr,
+                 List<Cabezal> cbs, int rpm, int ast, float lr,
                  int tl, int te, int dp) {
         this.cilindros = cs;
         this.capacidadDisco = cd;
@@ -44,7 +35,6 @@ public class Disco {
         this.tasaEscritura = tasaEscritura / 1000;
         this.diametroPlato = dp;
         //this.sectoresPorTrack
-		this.peticiones = null;
     }
 
     public int getTrackActual() {
@@ -72,63 +62,6 @@ public class Disco {
                 return c;
         }
         return null;
-    }
-
-    /*
-     * Modifica la variable 'peticiones'
-     */
-    public void leerPeticiones(String xml) {
-
-        try {
-            File file = new File(xml);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
-            NodeList petitions, blocks;
-            Element petE, blE;
-            Node pet, bl;
-
-            this.peticiones = new ArrayList<Peticion>();
-            Peticion in;
-            String prioridad, tipo;
-            int tiempo, bloque;
-
-            doc.getDocumentElement().normalize();
-
-            petitions = doc.getElementsByTagName("petition");
-
-            for (int i = 0; i < petitions.getLength(); i++) {
-
-                pet = petitions.item(i);
-                if (pet.getNodeType() == Node.ELEMENT_NODE) {
-
-                    petE = (Element) pet;
-
-                    prioridad = petE.getElementsByTagName("priority")
-                                .item(0).getTextContent();
-                    tiempo = Integer.parseInt(petE.getElementsByTagName("time")
-                                              .item(0).getTextContent());
-                    tipo = petE.getElementsByTagName("type")
-                           .item(0).getTextContent();
-
-                    in = new Peticion(tiempo, tipo, prioridad);
-
-                    blocks = petE.getElementsByTagName("block");
-
-                    for (int j = 0; j < blocks.getLength(); j++) {
-
-                        bl = blocks.item(j);
-                        bloque = Integer.parseInt(bl.getTextContent());
-
-                        in.addBloque(bloque);
-                    }
-
-                    peticiones.add(in);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private long efectuarAccion(int sector, char tipo) {
--- Simulador.java
+++ Simulador.java
@@ -2,19 +2,93 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.Collections;
 
+// Imports para lectura facil de XML
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class Simulador {
     Disco d;
     static int tamano_bloque = 4096;    //  Tamano de bloques de ext4
 
+	List<Peticion> peticiones; //  
+
     public Simulador(Disco d) {
         this.d = d;
+		this.peticiones = null;
     }
 
-    private Disco parseDisco(String archivo_config) {
-        //  Matteo
-        return null;
+    //private Disco parseDisco(String archivo_config) {
+        ////  Matteo
+        //return null;
+    //}
+
+    /*
+     * Modifica la variable 'peticiones'
+     */
+    public void leerPeticiones(String xml) {
+
+        try {
+            File file = new File(xml);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            NodeList petitions, blocks;
+            Element petE, blE;
+            Node pet, bl;
+
+            this.peticiones = new ArrayList<Peticion>();
+            Peticion in;
+            String prioridad, tipo;
+            int tiempo, bloque;
+
+            doc.getDocumentElement().normalize();
+
+            petitions = doc.getElementsByTagName("petition");
+
+            for (int i = 0; i < petitions.getLength(); i++) {
+
+                pet = petitions.item(i);
+                if (pet.getNodeType() == Node.ELEMENT_NODE) {
+
+                    petE = (Element) pet;
+
+                    prioridad = petE.getElementsByTagName("priority")
+                                .item(0).getTextContent();
+                    tiempo = Integer.parseInt(petE.getElementsByTagName("time")
+                                              .item(0).getTextContent());
+                    tipo = petE.getElementsByTagName("type")
+                           .item(0).getTextContent();
+
+                    in = new Peticion(tiempo, tipo, prioridad);
+
+                    blocks = petE.getElementsByTagName("block");
+
+                    for (int j = 0; j < blocks.getLength(); j++) {
+
+                        bl = blocks.item(j);
+                        bloque = Integer.parseInt(bl.getTextContent());
+
+                        in.addBloque(bloque);
+                    }
+
+                    peticiones.add(in);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+
+    /**
+      * Dado un numero de bloque devuelve el tiempo tomado por el disco duro 
+      * para leer o escribir dicho bloque
+      */
     private long procesarBloque(int bloque, char tipo) {
         int sector = d.buscarSectorParaBloque(bloque);
         return d.procesarSector(sector, tipo);
